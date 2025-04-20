7a4f32bc00b0e6c30d13129709907218d0f1cdab
Arian <ariyanfar81@gmail.com>
2025-04-20 22:45:11 +0330

    HUD

--- 2D_project/2d_fish/main.gd
+++ 2D_project/2d_fish/main.gd
@@ -4,7 +4,7 @@ extends Node
 var score
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,11 +15,15 @@ func _process(delta: float) -> void:
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")	
+	
 	
 	
 
@@ -52,6 +56,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
